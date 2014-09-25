1ffe694879dc88ca43d1cc9fa00c3aad68c222ab
aneesh-neelam <neelam.aneesh@gmail.com>
2014-09-26 00:11:58 +0530

    Algorithm 1
    Formula 1

--- JumbleCoding-Algorithm/Class1.cs
+++ JumbleCoding-Algorithm/Class1.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace JumbleCoding_Algorithm
 {
-    public class Class1
+    public class Jumbler
     {
+        public static string Jumble(string inputText)
+        {
+
+            StringBuilder outputText = new StringBuilder();
+            char character;
+
+            foreach (char str in inputText)
+            {
+                character = str;
+                if (character > 96 && character < 123)
+                {
+                    character = (char)((int)character + 12);
+                    if ((int)character > 122)
+                    {
+                        character = (char)(96 + ((int)character - 122));
+                    }
+                }
+                if (character > 64 && character < 91)
+                {
+                    character = (char)((int)character + 12);
+                    if (character > 90)
+                    {
+                        character = (char)(64 + ((int)character - 90));
+                    }
+                }
+                if (character > 47 && character < 58)
+                {
+                    character = (char)((int)character + 2);
+                    if (character > 57)
+                    {
+                        character = (char)(47 + ((int)character - 57));
+                    }
+                }
+                outputText = outputText.Append(character);
+            }
+            return outputText.ToString();
+        }
     }
 }
